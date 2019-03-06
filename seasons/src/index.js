c4930c3841c016aaf6ba8e12ea4ddb057e785496
aldosom <alexdobras@gmail.com>
2019-03-06 23:56:20 +0200

    Update index.js

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,14 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import SeasonDisplay from './SeasonDisplay';
+// import SeasonDisplay from './SeasonDisplay';
 
-const App = () => {
-  return(
-    <>
-      <h1>Seasons</h1>
-      <SeasonDisplay></SeasonDisplay>
-    </>
-  )
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lat: null,
+      errorMessage: ''
+    }
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        console.log(position);
+        this.setState({ lat: position.coords.latitude })
+      },
+      err => {
+        console.log(err);
+        this.setState({ errorMessage: err.message })
+      }
+    );
+  }
+  render() {
+    if (!this.state.lat && this.state.error) {
+      return <h1>>Error: {this.state.errorMessage}</h1>
+    }
+    if (this.state.lat && !this.state.error) {
+      return <h1>>Your latitude is: {this.state.lat}</h1>
+    }
+
+    return <h2>Loading...</h2>
+  }
 }
 
 ReactDOM.render(<App/>, document.querySelector("#root"));

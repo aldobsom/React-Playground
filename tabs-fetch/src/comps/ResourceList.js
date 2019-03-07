80dda79a22fd8636c10621e93dea441d6b11018d
aldosom <alexdobras@gmail.com>
2019-03-07 02:10:10 +0200

    Update ResourceList.js

--- tabs-fetch/src/comps/ResourceList.js
+++ tabs-fetch/src/comps/ResourceList.js
@@ -1,20 +1,25 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 
-export default ({resource}) => {
-    const [resources, setResources] = useState([]);
+const useResources = (resource) => {
+  const [resources, setResources] = useState([]);
 
-    useEffect( () => {
-      (async (resource) => {
-        const response = await axios.get(
-          `https://jsonplaceholder.typicode.com/${
-          resource
-          }`
-        );
-        setResources(response.data);
-      })(resource);
-    }, [resource])
-    
+  useEffect(() => {
+    (async (resource) => {
+      const response = await axios.get(
+        `https://jsonplaceholder.typicode.com/${
+        resource
+        }`
+      );
+      setResources(response.data);
+    })(resource);
+  }, [resource])
+
+  return resources;
+}
+
+export default ({resource}) => {
+    const resources = useResources(resource);
     return (
       <>
         <h1>

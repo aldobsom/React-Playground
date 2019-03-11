f407b022090acb98b26a384d730807c66ef9d373
aldosom <alexdobras@gmail.com>
2019-03-11 04:49:55 +0200

    not working

--- firbase-react-starter/src/App.js
+++ firbase-react-starter/src/App.js
@@ -1,28 +1,41 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import * as firebase from 'firebase';
 
-class App extends Component {
+class App extends React.Component {
+  state = {
+    speed: 11
+  }
+
+  componentDidMount() {
+    const rootRef = firebase.database().ref().child('test-e37bd');
+    console.log(rootRef);
+    const speedRef = rootRef.child("speed");
+     console.log(speedRef);
+    speedRef.on('value', snap => {
+      this.setState({speed: snap.val});
+      console.log(snap.val);
+    })
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    return this.state.speed;
   }
 }
+
+// const App = () => {
+//   const [speed, setSpeed] = useState(10);
+
+//   useEffect(()=>{
+//     const rootRef = firebase.database().ref().child('test-e37bd');
+//     const speedRef = rootRef.child("speed");
+//     speedRef.on('value', snap => {
+//       setSpeed(snap.val);
+//        console.log(snap.val);
+//     })
+//   }, [speed])
+
+//   return speed;
+// }
 
 export default App;
--- firbase-react-starter/src/index.js
+++ firbase-react-starter/src/index.js
@@ -3,6 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as firebase from 'firebase';
+// import firebase from 'firebase/app';
+// import 'firebase/firestore';
+// import 'firebase/auth';
+
+//data from firebase https://console.firebase.google.com/u/0/ 
+var config = {
+    apiKey: "AIzaSyAqFPy50CQeza25Fv5pwRdvXGNRQ0MaHKA",
+    authDomain: "test-e37bd.firebaseapp.com",
+    databaseURL: "https://test-e37bd.firebaseio.com",
+    projectId: "test-e37bd",
+    storageBucket: "test-e37bd.appspot.com",
+    messagingSenderId: "206705199287"
+}
+firebase.initializeApp(config);
+// firebase.firestore();
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 

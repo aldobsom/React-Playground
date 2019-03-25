5841e6b71d2995ad2c0baac441f23a83bd7a7e0e
aldosom <alexdobras@gmail.com>
2019-03-25 19:11:46 +0100

    Consumer method

--- translate-app-classes/src/components/Button.js
+++ translate-app-classes/src/components/Button.js
@@ -5,9 +5,20 @@ export default class Button extends React.Component {
 
   static contextType = LanguageContext;
 
+  renderSubmit(value) {
+    return value === 'english' ? "Submit" : "Voorlegen"
+  }
+
   render() {
-    console.log(this.context);
-    const text = this.context === 'english' ? "Submit" : "Voorleggen";
-    return <button className="ui button primary">{text}</button>
+    // console.log(this.context);
+    // const text = this.context === 'english' ? "Submit" : "Voorleggen";
+    
+    return (
+      <button className="ui button primary">
+        <LanguageContext.Consumer>
+          {(value) => this.renderSubmit(value)}
+        </LanguageContext.Consumer>
+      </button>
+    ) 
   }
 }
